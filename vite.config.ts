c56c1a41f094cbd507a4b71d5d5e0e4c599e25c9
shihabdelwar <shihabdelwar@gmail.com>
2025-12-25 15:34:41 -0500

    Shihab

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
 
 export default defineConfig({
-  base: '/cs-portfolio/',
+  base: '/Portfolio-Website/',
   plugins,
   resolve: {
     alias: {
